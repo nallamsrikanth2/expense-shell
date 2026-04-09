8f5f8f10bda014d64991fef6587a8e7543890635
nallamsrikanth2 <nallamsrikanth12>
2026-04-10 00:55:27 +0530

    dbapplication

---
+++ my-sqldb.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$TIMESTAMP-$SCRIPT_NAME.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[33m"
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+        echo -e "$R $2 is failure $N"
+    else
+        echo -e "$G $2 is success $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then    
+    echo -e " $R please run this script inside the root user $N"
+else
+    echo -e "$G you are in root user $N"
+fi
+
+dnf install mysql-server -y
+VALIDATE $? "installingvmysql server"
+
+systemctl enable mysqld
+VALIDATE $? "enabling the mysqld"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "set the root password"
+
+
+
+
+
